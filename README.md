577cec35e650a1567685146573330cd80299bf2e
Ekemini <42721358+b4ssey@users.noreply.github.com>
2022-10-18 12:37:02 +0100

    Public repo for week 2

---
+++ README.md
@@ -0,0 +1,21 @@
+# Cloud Module Application
+
+This is an web application written in Python. It does things and we'd like help deploying it. Forked from Lorenzo's first challenge task.
+
+## Instructions
+
+1. Fork this repo, for deployment on AWS.
+2. Change both 'localhosts' to IP address of ec2 instances for API1 & API2.
+3. Chang YOUR_NAME in HTML in homepage.html.
+4. Sucessfully configure your flask app to your 1st & 2nd api.
+5. Continue with parent task.
+
+### Notes on the challenges
+
+- If they are any bugs found in this repo, I would be lovely to hear from you or furthermore fix it for the team!
+- Work with the free-tier systems and instances supplied by AWS.
+- Keep Security groups as simple as possible. HTTP for now.
+
+### Conclusion
+
+I am open to questions and pull requests. Happy hacking. Cheers.
---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+from random import randint
+from time import time
+
+from helpers.numbers_api import get_fact_for_number
+from helpers.first_api import get_lucky_number
+from helpers.second_api import get_worst_food
+
+app = Flask(__name__)
+
+WEEK2_FIRST_API = "localhost:5000/"
+WEEK2_SECOND_API = "localhost:5001/"
+
+@app.route("/")
+def homepage():
+    current_epoch = time()
+    random_number = randint(0, 10000)
+    lucky = get_lucky_number(WEEK2_FIRST_API, "luckynumber")
+    worst = get_worst_food(WEEK2_FIRST_API,"worstfoodfrom1", "Something went wrong, fix and try again.")
+    worstsource = get_worst_food(WEEK2_SECOND_API,"worstfoodfrom2", "Unavailable, Great Job!")
+
+    return render_template("homepage.html", number=random_number, time=current_epoch, lucky=lucky, worst=worst, worstsource=worstsource )
+
+
+@app.route("/number-fact")
+def number_fact():
+    random_number = randint(0, 100)
+    fact = get_fact_for_number(random_number)
+
+    return render_template("number-fact.html", number=random_number, fact=fact)
