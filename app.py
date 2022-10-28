5829ba2cb8f9f676358890b98336cf9c6b247322
Sharmaine <sharmainetaylor@ymail.com>
2022-10-28 21:47:01 +0100

    Updated files

--- app.py
+++ app.py
@@ -8,18 +8,21 @@ from helpers.second_api import get_worst_food
 
 app = Flask(__name__)
 
-WEEK2_FIRST_API = "http://18.170.118.40:5000/"
-WEEK2_SECOND_API = "http://13.40.128.193:5001/"
+WEEK2_FIRST_API = "18.170.118.40:5000/"
+WEEK2_SECOND_API = "13.40.128.193:5001/"
+
 
 @app.route("/")
 def homepage():
     current_epoch = time()
     random_number = randint(0, 10000)
     lucky = get_lucky_number(WEEK2_FIRST_API, "luckynumber")
-    worst = get_worst_food(WEEK2_FIRST_API,"worstfoodfrom1", "Something went wrong, fix and try again.")
-    worstsource = get_worst_food(WEEK2_SECOND_API,"worstfoodfrom2", "Unavailable, Great Job!")
+    worst = get_worst_food(WEEK2_FIRST_API, "worstfoodfrom1",
+                           "Something went wrong, fix and try again.")
+    worstsource = get_worst_food(
+        WEEK2_SECOND_API, "worstfoodfrom2", "Unavailable, Great Job!")
 
-    return render_template("homepage.html", number=random_number, time=current_epoch, lucky=lucky, worst=worst, worstsource=worstsource )
+    return render_template("homepage.html", number=random_number, time=current_epoch, lucky=lucky, worst=worst, worstsource=worstsource)
 
 
 @app.route("/number-fact")
